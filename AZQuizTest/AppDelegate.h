ab8b3f62101ff24e32e76f0ce3f1884728d47b79
Andrew Zeniukevich <del.exe@gmail.com>
2018-04-10 18:43:56 +0200

    Initial Commit

---
+++ AZQuizTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AZQuizTest
+//
+//  Created by andrei zeniukevich on 10/04/2018.
+//  Copyright © 2018 andrei zeniukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ AZQuizTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AZQuizTest
+//
+//  Created by andrei zeniukevich on 10/04/2018.
+//  Copyright © 2018 andrei zeniukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
